9e5f2f116749f11a69f5683ec2de0e7a6504826b
D sakai <yukkurino.9@gmail.com>
2021-02-01 12:07:51 +0900

    多分完成

--- EX6/Box.h
+++ EX6/Box.h
@@ -2,73 +2,145 @@
 
 class Pack {
 public:
-	virtual double GetPackSize() = 0;
-	virtual void InputSize() = 0;
+	//形で変わる
+	virtual double GetPackSize() = 0; //サイズの取得、派生先で実装を必須にする、形に大路て実装
+
+	//形で変わる
+	virtual void InputSize() = 0;     //サイズの入力、派生先で実装を必須にする、形に大路て実装
+
+	//重さ
+	virtual double GetPackWeight() = 0;
 };
 
+//ここが箱の大きさ
 class Box:public Pack {
-public :
-	double width;
-	double height;
-	double depth;
-	double GetPackSize() override {
+public : //宣言フェイズ
+	double width;  // 幅
+	double height; // 高さ
+	double depth;  // 奥行
+
+	double weight; // 重さ
+	
+	double GetPackSize() override { //派生元の宣言されたVirtualを再定義する
 		return width + height + depth;
 	}
-	Box(double w=0,double h=0, double d=0) {
+
+	double GetPackWeight() override {//派生元の宣言されたVirtualを再定義する
+		return weight;
+	}
+
+	Box(double w=0,double h=0, double d=0 , double we = 0) {
 		width = w;
 		height = h;
 		depth = d;
+
+		weight = we;
 	}
-	void InputSize() override {
-		std::cout << "幅を入力";
+
+	//classの処理部分
+	void InputSize() override { //派生元の宣言されたVirtualを再定義する
+		std::cout << "幅を入力(cm)";
 		std::cin >> width;
-		std::cout << "高さを入力";
+		std::cout << "高さを入力(cm)";
 		std::cin >> height;
-		std::cout << "奥行きを入力:";
+		std::cout << "奥行きを入力(cm)";
 		std::cin >> depth;
+		
+		std::cout << "重さを入力(kg)";
+		std::cin >> weight;
 	}
+
 };
 
+//ここが円柱の大きさ
 class Cylinder :public Pack {
-public:
-	double radius;
-	double height;
-	double GetPackSize() override {
+public: //宣言フェイズ
+	double radius; // 半径
+	double height; // 高さ
+
+	double weight; // 重さ
+
+	double GetPackSize() override {//派生元の宣言されたVirtualを再定義する
 		return radius * 4 + height;
 	}
-	void InputSize() override {
-		std::cout << "半径を入力";
+
+	double GetPackWeight() override {//派生元の宣言されたVirtualを再定義する
+		return weight;
+	}
+
+	Cylinder(double r = 0, double h = 0, double we = 0) {
+		radius = r;
+		height = h;
+
+		weight = we;
+	}
+
+	//classの処理部分
+	void InputSize() override {//派生元の宣言されたVirtualを再定義する
+		std::cout << "半径を入力(cm)";
 		std::cin >> radius;
-		std::cout << "高さを入力";
+		std::cout << "高さを入力(cm)";
 		std::cin >> height;
+
+		std::cout << "重さを入力(kg)";
+		std::cin >> weight;
 	}
 };
 
-
+//ここで区別された宅配の会社のデータ（大きさなど）を受け取る。＊１
 class PackSizeList {
-public:
+public: //宣言フェイズ
 	double* size;
-	unsigned int length;
-	PackSizeList(double s[],unsigned int l) {
+	unsigned int sizeLength;
+
+	double* weight;
+	unsigned int weightLength;
+
+	PackSizeList(double s[],unsigned int ls , double w[] , unsigned int lw) {
 		size = s;
-		length = l;
+		sizeLength = ls;
+
+		weight = w;
+		weightLength = lw;
 	}
 };
 
 
 class Takuhai{
-public:
+public: //宣言フェイズ
 	Pack* pack;
-	PackSizeList* packSizeList;
+	PackSizeList* packSizeList; //ここにきてる。＊１
+
+	//ここで区別された宅配の会社のデータから大きさを区別している。
 	double GetPackSize() {
-		double judge = 0;
-		for (int i = 0; i < packSizeList->length; i++) {
-			if (pack->GetPackSize() <= packSizeList->size[i]) {
-				judge = packSizeList->size[i];
+		double judgeSize = 0;
+
+		//サイズ
+		for (int i = 0; i < packSizeList->sizeLength; i++) {
+			if (pack->GetPackSize() <= packSizeList->size[i])
+			{
+				judgeSize = packSizeList->size[i];
 				break;
 			}
 		}
-		return judge;
+
+		return judgeSize;
+	}
+
+	double GetPackWeight() {
+		bool judgeWeight = false;
+
+		//重さ
+		for (int w = 0; w < packSizeList->weightLength; w++) {
+			if (pack->GetPackWeight() <= packSizeList->weight[w])
+			{
+				judgeWeight = true;
+				break;
+			}
+		}
+
+		return judgeWeight;
+
 	}
 };
 
--- EX6/ex6.cpp
+++ EX6/ex6.cpp
@@ -7,11 +7,11 @@ unsigned int InputUINT(string message, unsigned int max) {
 	while (true) {
 		cout << message;
 		cin >> i;
-		if (i < max) {
+		if (i < max) { //最大より大きかったら入力エラーと出る。
 			break;
 		}
 		else {
-			cout << "入力エラー";
+			cout << "入力エラー" << endl;
 		}
 	}
 	return i;
@@ -21,13 +21,21 @@ int main()
 {
 	//クロネコのサイズ https://www.kuronekoyamato.co.jp/ytc/search/payment/size/
 	double kuronekoSize[] = { 60,80,100,120,160 };
+	//クロネコの重さ
+	double kuronekoWeight[] = { 2 ,5,10,15,20,25 };
 
 	//JPのサイズ https://www.post.japanpost.jp/send/fee/kokunai/parcel.html#01
 	double jpSize[] = { 60,80,100,120,140,160,170 };
+	double jpWeight[] = { 25 };
+
+	//佐川急便のサイズ https://www.sagawa-exp.co.jp/send/fare/list/sagawa_faretable/attention.html
+	double sagawaSize[] = { 60,80,100,140,160,170,180,200,220,240,260 };
+	double sagawaWeight[] = { 2,5,10,20,30 };
 
 	unsigned int i = InputUINT("形は？（0:箱型 1:円柱)", 2);
+
 	Pack* pack =nullptr;
-	switch (i) {
+	switch (i) { //ここでサイズの指定できるclassへ移動する
 	case 0:
 		pack =  new Box();
 		break;
@@ -42,22 +50,37 @@ int main()
 	Takuhai takuhai = Takuhai();
 	takuhai.pack = pack;
 
-	i = InputUINT("業者は？（0:クロネコ 1:JP)", 2);
-	switch (i) {
-	case 0:
-		takuhai.packSizeList = new PackSizeList(kuronekoSize,_countof( kuronekoSize));
+	//ここで宅配の会社を区別している。
+	i = InputUINT("業者は？（0:クロネコ 1:JP 2:佐川)", 3);
+	switch (i) { //区別した会社ごとのデータを搬送
+	case 0: 
+		takuhai.packSizeList = new PackSizeList(kuronekoSize,_countof( kuronekoSize) , kuronekoWeight, _countof(kuronekoWeight));
 		break;
 	case 1:
-		takuhai.packSizeList = new PackSizeList(jpSize, _countof(jpSize));
+		takuhai.packSizeList = new PackSizeList(jpSize, _countof(jpSize) , jpWeight,_countof(jpWeight));
+		break;
+	case 2 :
+		takuhai.packSizeList = new PackSizeList(sagawaSize, _countof(sagawaSize) , sagawaWeight,_countof(sagawaWeight));
 		break;
 	default:
 		break;
 	}
+
+	//ここで大きさを区別するclassに移動
 	double packSize = takuhai.GetPackSize();
-	if (packSize != 0) {
-		cout << "宅配のサイズは" << packSize;
+	bool packWeight = takuhai.GetPackWeight();
+
+	if (packSize != 0 && packWeight)
+	{
+		cout << "宅配のサイズは" << packSize << endl;
+		cout << "宅配物の重さは" << pack->GetPackWeight() << "(kg)" << endl;
 	}
-	else {
+	else if (packSize != 0 && !packWeight) 
+	{
+		cout << "この重さで宅配は送れません" << endl;
+	}
+	else 
+	{
 		cout << "宅配では送れません";
 	}
 
