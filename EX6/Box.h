dba42b984a73dfd05c33bb0a66c01bea42514a2c
D sakai <yukkurino.9@gmail.com>
2021-02-03 14:40:52 +0900

    新しい奴

--- EX6/Box.h
+++ EX6/Box.h
@@ -10,12 +10,12 @@ public:
 		std::cin >> weight;
 	}
 
-	double GetWeight() 
+	double GetWeight()
 	{
 		return weight;
 	}
 
-	void Input() 
+	void Input()
 	{
 		InputSize();
 		InputWeight();
@@ -29,17 +29,17 @@ public:
 };
 
 //ここが箱の大きさ
-class Box:public Pack {
-public : //宣言フェイズ
+class Box :public Pack {
+public: //宣言フェイズ
 	double width;  // 幅
 	double height; // 高さ
 	double depth;  // 奥行
-	
+
 	double GetPackSize() override { //派生元の宣言されたVirtualを再定義する
 		return width + height + depth;
 	}
 
-	Box(double w=0,double h=0, double d=0)// 変数=0はなにも無いときは零が中に入る。
+	Box(double w = 0, double h = 0, double d = 0)// 変数=0はなにも無いときは零が中に入る。
 	{
 		width = w;
 		height = h;
@@ -68,7 +68,7 @@ public: //宣言フェイズ
 		return radius * 4 + height;
 	}
 
-	Cylinder(double r = 0, double h = 0){
+	Cylinder(double r = 0, double h = 0) {
 		radius = r;
 		height = h;
 	}
@@ -94,14 +94,14 @@ public: //宣言フェイズ
 	PackSet* packSet; //配列を受け取る
 	unsigned int Length;
 
-	PackSizeList(PackSet p[],unsigned int l) {
+	PackSizeList(PackSet p[], unsigned int l) {
 		packSet = p;
 		Length = l;
 	}
 };
 
 //業者の荷物を表す。
-class Takuhai{
+class Takuhai {
 public: //宣言フェイズ：PackSizeListとPackの参照を持つ
 	Pack* pack;
 	PackSizeList* packSizeList; //ここにきてる
